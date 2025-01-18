c996bcd27f535de8bc698492f3185cff2de69b54
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2025-01-18 16:01:11 +0100

    Updated website

--- portfolio-leon-bos/src/App.js
+++ portfolio-leon-bos/src/App.js
@@ -4,14 +4,14 @@ import React, { useState, useEffect } from 'react';
 function ExperienceCard({ role, dates, company, description, technologies, grade }) {
   return (
     <div className="relative">
-      <div className="p-10 bg-gray-800 text-white rounded-2xl shadow-lg">
-        <h3 className="text-2xl font-semibold">{role}</h3>
-        <p className="text-sm text-gray-400">{dates}</p>
-        <p className="mt-4 font-bold">Company: {company}</p>
+      <div className="p-10 text-black rounded-2xl shadow-2xl">
+        <h3 className="text-xl md:text-2xl font-semibold">{role}</h3>
+        <p className="text-sm md:text-base text-gray-800">{dates}</p>
+        <p className="text-sm md:text-base mt-4 font-bold">Company: {company}</p>
         {grade &&
-          <p className="mb-2 font-bold">Grade: {grade}</p>
+          <p className="text-sm md:text-base mb-2 font-bold">Grade: {grade}</p>
         }
-        <p> {description} </p>
+        <p className='text-sm md:text-base'> {description} </p>
         {/* Display Technologies */}
         <div className="mt-4">
           <h4 className="text-sm font-semibold">Technologies:</h4>
@@ -37,16 +37,16 @@ function ExperienceCard({ role, dates, company, description, technologies, grade
 
 function SkillsCard({ skill, imagePath }) {
   return (
-    <div className="bg-white p-4 shadow-md rounded-2xl">
-      <img className='w-[20%]' alt='.NET' src={imagePath} />
-      {skill}
+    <div className="bg-white p-6 shadow-2xl rounded-2xl flex flex-col items-center text-center">
+      <img className="w-16 h-16 mb-4" alt={skill} src={imagePath} />
+      <p className="text-lg font-semibold text-gray-800 mt-6">{skill}</p>
     </div>
-  )
-};
+  );
+}
 
 function ProjectCard({ name, description, githubUrl }) {
   return (
-    <div className='bg-white p-6 shadow-md rounded-2xl relative pb-16'>
+    <div className='bg-white p-6 shadow-2xl rounded-2xl relative pb-16'>
       <h4 className='text-xl font-bold'>{name}</h4>
       <p>{description}</p>
       <a
@@ -86,11 +86,11 @@ function ExperienceCounter() {
   }, []);
 
   return (
-    <div className="experience-counter text-center py-10 flex items-center justify-center space-x-4 shadow-md bg-white rounded-2xl mx-auto">
-      <h2 className="text-3xl font-semibold">
+    <div className="experience-counter text-center py-10 flex flex-wrap md:flex-nowrap items-center justify-center space-x-4 shadow-md bg-white rounded-2xl mx-auto">
+      <h2 className="text-xl md:text-3xl font-semibold">
         {time.years} years, {time.months} months, {time.days} days and {time.seconds} seconds
       </h2>
-      <h2 className="text-3xl">experience in software engineering</h2>
+      <h2 className="text-xl md:text-3xl">experience in software engineering</h2>
     </div>
   );
 }
@@ -149,7 +149,7 @@ function App() {
       <section className="relative text-white text-center py-40 overflow-hidden">
         <div className="absolute inset-0">
           <img
-            src="bg.jpg"
+            src="bg.png"
             alt="Background"
             className="w-full h-full object-cover"
           />
@@ -157,7 +157,7 @@ function App() {
         </div>
         <div className="relative z-10">
           <h2 className="text-4xl font-bold mb-4">Hi, I'm Leon Bos</h2>
-          <p className="text-xl">Insert some quote or something here</p>
+          <p className="text-xl">Software Engineer</p>
         </div>
       </section>
 
@@ -166,33 +166,56 @@ function App() {
 
       {/* About Section */}
       <section id="about" className="container mx-auto p-10">
-        <h3 className="text-3xl font-bold mb-4">About Me</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt lobortis lobortis. Maecenas volutpat auctor porttitor. Proin tincidunt scelerisque malesuada. Donec ut pulvinar lacus, sed interdum dui. Ut fringilla dignissim bibendum. Nullam ut lacus elementum, efficitur felis at, dictum enim. Aenean quis suscipit mi. Duis in justo tristique risus feugiat aliquam quis id elit. Aliquam arcu elit, aliquam nec nulla sed, volutpat vestibulum nibh. Morbi sed hendrerit sem, eu pulvinar nulla. Praesent nec purus finibus ligula gravida placerat non bibendum nisl. </p>
+        <div className='bg-white p-6 shadow-2xl rounded-2xl relative pb-16'>
+          <h3 className="text-xl md:text-3xl font-bold mb-6">About Me</h3>
+          <div className="text-sm md:text-base leading-relaxed">
+            <p>
+              Hello, I'm <strong>Leon Bos</strong>, a 23-year-old software engineer based in Delfzijl, the Netherlands. I hold a bachelor's degree in ICT Software Engineering from Hanze University of Applied Sciences, Groningen. Currently, I work as a <strong>Young Professional Software Engineer</strong> at <strong>RDW</strong>, contributing to the EUCARIS service team.
+            </p>
+            <p className="mt-4">
+              At EUCARIS, I work as part of a dedicated team developing and maintaining software systems that enable seamless vehicle data exchange between European countries. My responsibilities include implementing, and optimizing solutions using technologies such as <strong>.NET</strong>, <strong>Azure</strong>, and <strong>Entity Framework</strong>. I collaborate closely with stakeholders to ensure compliance with international standards and to support secure and efficient data sharing for cross-border vehicle registration, taxation, and law enforcement purposes.
+            </p>
+            <p className="mt-4">
+              Outside of work, I enjoy working on hobby software projects and am passionate about fitness and powerlifting. I'm a <strong>curious</strong> and <strong>disciplined</strong> individual who constantly strives for self-improvement. I thrive in collaborative environments and pride myself on being a <strong>team player</strong>.
+            </p>
+          </div>
+        </div>
       </section>
 
       {/* Skills Section */}
       <section id="skills" className="container mx-auto p-10">
-        <h3 className="text-3xl font-bold mb-4">Skills</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          <SkillsCard skill=".NET" imagePath="dotnet.png" />
-          <SkillsCard skill="Azure" imagePath="azure.png" />
-          <SkillsCard skill="Python" imagePath="python.png" />
-          <SkillsCard skill="React" imagePath="react.png" />
+        <div className='bg-white p-6 relative pb-16'>
+          <h3 className="text-xl md:text-3xl font-bold mb-6">Skills</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            <SkillsCard skill=".NET" imagePath="dotnet.png" />
+            <SkillsCard skill="Azure" imagePath="azure.png" />
+            <SkillsCard skill="Python" imagePath="python.png" />
+            <SkillsCard skill="React" imagePath="react.png" />
+            <SkillsCard skill="Scrum" imagePath="scrum.png" />
+            <SkillsCard skill="Vue" imagePath="vue.png" />
+            <SkillsCard skill="Blazor" imagePath="blazor.png" />
+            <SkillsCard skill="SQL Server" imagePath="sql.png" />
+            <SkillsCard skill="Docker" imagePath="docker.png" />
+          </div>
         </div>
       </section>
 
       {/* Experience Section */}
       <section id="experiences" className="container mx-auto p-10">
-        <h2 className="text-4xl font-bold text-center mb-8">Experience</h2>
+        <h2 className="text-xl md:text-3xl font-bold text-center mb-8">Experience</h2>
         <div className="space-y-10">
           <ExperienceCard
             role="Junior Software Developer"
             dates="October 2022 - February 2024"
             company="Bencom Group"
             description={[
-              'Worked on a number of price comparison websites. Mainly did backend development with some cloud development in Azure',
+              'Worked on several price comparison websites, focusing primarily on backend development. Contributed to cloud infrastructure and solutions using Azure, ensuring scalability and efficiency across multiple projects.',
             ]}
             technologies={[
+              {
+                'image': 'python.png',
+                'description': 'Python'
+              },
               {
                 'image': 'dotnet.png',
                 'description': '.NET'
@@ -213,7 +236,7 @@ function App() {
             company="Bencom Group"
             grade="9.0/10"
             description={[
-              'Developed a new "Postcode availability check" for internetten.nl. First experience building a new project in .NET and first experience putting a production application in the cloud with Azure',
+              'Designed and implemented a "Postcode Availability Check" feature for internetten.nl. This project marked my first experience developing a new application in .NET and deploying a production-ready solution in the cloud using Azure.',
             ]}
             technologies={[
               {
@@ -223,6 +246,10 @@ function App() {
               {
                 'image': 'azure.png',
                 'description': 'Azure'
+              },
+              {
+                'image': 'ef.png',
+                'description': 'Entity Framework'
               }
             ]}
           />
@@ -232,7 +259,7 @@ function App() {
             company="RDW"
             grade="9.0/10"
             description={[
-              'Created a Proof of concept of a machine learning model to extract Vehicle Identification Numbers out of pictures. Can be used to automate the process of importing cars to The Netherlands. Helps to save a lot of manual labor, 250.000 cars are imported every year, 3 pictures have to be validated per car',
+              'Developed a proof of concept for a machine learning model to extract Vehicle Identification Numbers (VINs) from images. This automation can streamline the importation process of vehicles into The Netherlands, reducing the need for manual validation of approximately 750,000 images annually.',
             ]}
             technologies={[
               {
@@ -258,7 +285,7 @@ function App() {
             dates="September 2024 - Now"
             company="RDW"
             description={[
-              'Started working as a software engineer at RDW in the Young Professional program. Working with team Eucaris, which is a team working on software to make vehicle data exchange possible between European countries',
+              'Joined RDW as part of the Young Professional program. Collaborating with the Eucaris team, I develop software solutions that enable secure and efficient vehicle data exchange between European countries.',
             ]}
             technologies={[
               {
@@ -276,7 +303,7 @@ function App() {
 
       {/* Projects Section */}
       <section id="projects" className="container mx-auto p-10">
-        <h3 className="text-3xl font-bold mb-4">Hobby Projects</h3>
+        <h3 className="text-xl md:text-3xl font-bold mb-4">Hobby Projects</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <ProjectCard githubUrl="https://github.com/leonbos1/gainz-workout-app" name="Workout App" description="A fitness tracking app developed in React Native to monitor and enhance personal fitness progress. This project involved mastering app development and gaining hands-on experience with database integration to store and retrieve workout data efficiently." />
           <ProjectCard githubUrl="https://github.com/leonbos1/occasion-scraper" name="Occassion Scraper" description="A web scraping application designed to monitor car selling websites at regular intervals and send results via email. Features include a frontend and backend for visualizing, filtering, and managing car listings. Users can create and subscribe to blueprints, custom filters that notify them of specific car listings when they become available. Developed using Selenium, Vue.js, and Flask." />
@@ -290,8 +317,21 @@ function App() {
 
       {/* Contact Section */}
       <section id="contact" className="container mx-auto p-10">
-        <h3 className="text-3xl font-bold mb-4">Contact Me</h3>
-        <p>If you'd like to get in touch, email me at <a href="mailto:your.email@example.com" className="text-blue-600 hover:underline">bos.leon2001@gmail.com</a>.</p>
+        <div className='bg-white p-6 shadow-2xl rounded-2xl relative pb-16'>
+          <h3 className="text-xl md:text-3xl font-bold mb-6">Contact Me</h3>
+          <p className="mb-6">Feel free to reach out to me through email or connect with me on LinkedIn!</p>
+          <div className="flex justify-center space-x-6">
+            {/* Email */}
+            <a href="mailto:bos.leon2001@gmail.com" className="flex items-center space-x-2 hover:text-blue-600">
+              <img src="email.png" alt="Email" className="w-8 h-8" />
+              <span className="text-lg font-medium">bos.leon2001@gmail.com</span>
+            </a>
+            {/* LinkedIn */}
+            <a href="https://www.linkedin.com/in/leon-bos-0888a7215/" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 hover:text-blue-600">
+              <img src="linkedin.png" alt="LinkedIn" className="w-8 h-8" />
+              <span className="text-lg font-medium">Leon Bos</span>
+            </a>
+          </div>        </div>
       </section>
 
       {/* Footer */}
@@ -300,8 +340,6 @@ function App() {
       </footer>
     </div>
   );
-
-
 }
 
 export default App;
